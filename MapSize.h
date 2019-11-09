433064d4a265b343fbf980cfe50dfd0eab8958db
Emmyirab-anon <57382198+Emmyirab-anon@users.noreply.github.com>
2019-11-08 18:08:30 -0600

    Update MapSize.h

--- MapSize.h
+++ MapSize.h
@@ -1,10 +1,3 @@
-/* Name: Emmanuel Efewongbe
-   Student ID: 200417647
-   Course: CS 115
-   Classs section number: 002
-   Lab section number: 083
-*/
-
 #pragma once
 const int MAP_SIZE_SOUTH = 20;
 const int MAP_SIZE_EAST = 40;
